d5f2558774eff9212a323094a4affb11bd1dacfa
Leyder Quintero <lele.quinter@gmail.com>
2022-01-22 17:30:29 -0500

    Impresionde variables en el HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,10 +2,12 @@
 import React from 'react';
 
 const PrimeraApp = () =>{
-
+    const saludo = 'Hola Mundo'
     return (
+        //se elimino el Fragment        
         <>
-            <h1>Hola mundo</h1>
+            <h1> { saludo } </h1>
+            {/* <pre> { JSON.stringify( saludo, null, 3 ) } </pre> */}
             <p>Parrafo de la aplicacion</p>
         </>        
     );
