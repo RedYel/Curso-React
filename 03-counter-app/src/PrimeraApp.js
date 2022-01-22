556c87df939028ac9437f5bee2d3bb65b50a5cbb
Leyder Quintero <lele.quinter@gmail.com>
2022-01-22 17:09:48 -0500

    Explicacion de retornar componente-fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 const PrimeraApp = () =>{
 
-    return <h1>Hola mundo</h1>
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Parrafo de la aplicacion</p>
+        </>        
+    );
     
 }
+
 
 export default PrimeraApp;
