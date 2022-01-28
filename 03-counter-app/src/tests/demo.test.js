b136615849979029ec43301cf47259e859dd68df
Leyder Quintero <lele.quinter@gmail.com>
2022-01-27 22:52:45 -0500

    Seccion #5 pruebas unitarias y de integracion, se añadió una carpeta llamada test y se realizó la primera prueba oficial

---
+++ 03-counter-app/src/tests/demo.test.js
@@ -0,0 +1,18 @@
+
+// test( 'esta es mi primera prueba, debe ser true', () => {
+//     const isActive = true;
+
+//     if ( isActive ){
+//         throw new Error('No esta activo');
+//     }
+
+// } )
+
+test('deben de ser iguales los strings', () => {
+  // 1. inicializacion
+  const mensaje = 'Hola mundo';
+  // 2. estimulo
+  const mensaje2 = `Hola mundo`;
+  // 3. Observar el comportamaiento
+  expect( mensaje ).toBe( mensaje2 );
+});
