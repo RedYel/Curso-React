629105dd216e881c85208cac2437bc5596262773
Leyder Quintero <lele.quinter@gmail.com>
2022-04-24 19:54:19 -0500

    Preparacion de las routes, coonfiguracion de react router, navBar creada

---
+++ 05-hook-app/src/components/09-useContext/AppRouter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AboutScreen } from "./AboutScreen";
+import { HomeScreen } from "./HomeScreen";
+import { LoginScreen } from "./LoginScreen";
+import { NavBar } from "./NavBar";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar></NavBar>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/about" element={<AboutScreen />} />
+          <Route exact path="/login" element={<LoginScreen />} />
+          <Route path="*" element={<HomeScreen />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
