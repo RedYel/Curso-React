5bc9ef8b813d966542b811d2e98e898176a421a4
Leyder Quintero <lele.quinter@gmail.com>
2022-04-24 20:14:57 -0500

    createContext y useContext

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import { AppRouter } from './AppRouter'
+import React from "react";
+import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
+    const user = {
+        id: 123,
+        name: 'LeyderQuintero',
+        email: 'leler@gmail.com',
+    }
   return (
-        <AppRouter/>
-  )
-}
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
+};
