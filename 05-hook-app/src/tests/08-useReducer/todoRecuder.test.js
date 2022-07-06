fe0d0788e50fcd37361e2d965b9e2d851ba0b080
Leyder Quintero <lele.quinter@gmail.com>
2022-07-06 11:20:58 -0500

    pruebas de realizar toggle y eliminar todo

--- 05-hook-app/src/tests/08-useReducer/todoRecuder.test.js
+++ 05-hook-app/src/tests/08-useReducer/todoRecuder.test.js
@@ -18,14 +18,37 @@ describe("Preubas todoREducer", () => {
   test("Debe agregar ToDo", () => {
     const action = {
       type: "add",
-      payload: [{
-        id: 2,
-        description: "nuevo Todo #2",
-        done: false,
-      }],
+      payload: [
+        {
+          id: 2,
+          description: "nuevo Todo #2",
+          done: false,
+        },
+      ],
     };
 
-    const newState = todoReducer(initialState, {});
-    expect(newState.length).toBe(1);
+    const newState = todoReducer(initialState, action);
+    expect(newState.length).toBe(2);
+    expect(newState).toContain(action.payload);
+  });
+
+  test("Debe eliminar un todo", () => {
+    const action = {
+      type: "delete",
+      payload: 1,
+    };
+
+    const newState = todoReducer(initialState, action);
+    expect(newState.length).toBe(0);
+  });
+
+  test("Debe de realizar el Toogle del ToDo", () => {
+    const action = {
+      type: "toggle",
+      payload: 1,
+    };
+
+    const newState = todoReducer(initialState, action);
+    expect(newState[0].done).toBe(true);
   });
 });
