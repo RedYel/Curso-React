db098645c496ff74aad838572192e2fdaedff51a
Leyder Quintero <lele.quinter@gmail.com>
2022-10-02 18:48:08 -0500

    Login de un usuario

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useReducer } from "react";
+import { types } from "../types/types";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
@@ -10,5 +11,25 @@ const initialState = {
 export const AuthProvider = ({ children }) => {
   const [authState, dispatch] = useReducer(authReducer, initialState);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const login = async (name = "") => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: "abc",
+        name: name,
+      },
+    };
+    dispatch(action);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...authState,
+        login: login,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
