8d06773b401f85fe1cb9076a95092bae58144641
Leyder Quintero <lele.quinter@gmail.com>
2022-10-02 20:12:41 -0500

    Rutas públicas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -2,13 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         {/* Rutas de Login */}
-        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
         {/* Cualquier ruta que no sea el login desplegarà el componente de HeroesRoutes */}
         <Route
           path="/*"
@@ -18,6 +26,7 @@ export const AppRouter = () => {
             </PrivateRoute>
           }
         />
+        {/* <Route path="login" element={<LoginPage />} /> */}
         {/* <Route path="/*" element={} /> */}
       </Routes>
     </>
